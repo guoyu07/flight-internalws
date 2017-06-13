6d3bd18b6281e5ef83e2573b151e6e767f406528
kyxie <kyxie@Ctrip.com>
2017-06-13 22:12:11 +0800

    新增API客户端代码，repository层增加rpc client和server依赖，调用API接口成功

---
+++ flight-internalws-respository/src/test/java/com/ctrip/ibu/flight/internalws/repository/ws/TestAPI.java
@@ -0,0 +1,31 @@
+package com.ctrip.ibu.flight.internalws.repository.ws;
+
+import com.ctrip.soa.global.api.orderdetailsearch.v1.GaOrderDetailSearchRequest;
+import com.ctrip.soa.global.api.orderdetailsearch.v1.GaOrderDetailSearchResponse;
+import com.ctrip.soa.international.ibuflightapi.service.v1.IBUFlightAPIClient;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by kyxie on 2017/6/13.
+ */
+public class TestAPI extends TestCase {
+
+    @Test
+    public void testApi(){
+        IBUFlightAPIClient client = IBUFlightAPIClient.getInstance("http://ws.flight.ibu.fat55.qa.nt.ctripcorp.com/intlflightapi");
+        Assert.assertNotNull(client);
+
+        GaOrderDetailSearchResponse orderDetail = null;
+        GaOrderDetailSearchRequest request = new GaOrderDetailSearchRequest();
+
+        try{
+            orderDetail = client.gaOrderDetailSearch(request);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Assert.assertNotNull(orderDetail);
+    }
+}
